1125c50050a11d4e11a107fc82f69c5dd8d5ab5e
Nickbis <nickbis3@gmail.com>
2022-10-18 00:21:48 +0300

    feat: add simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  currChain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.currChain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.currChain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (this._checkIntegerNumber(position)) {
+      if (position < 1 || position > this.getLength()) {
+        this._deleteChain();
+        throw new Error('You can\'t remove incorrect link!');
+      }
+      const index = position -1;
+      this.currChain.splice(index, 1);
+      return this;
+    }
+    this._deleteChain();
+    throw new Error('You can\'t remove incorrect link!');
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.currChain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const chain = this._getChain();
+    this._deleteChain();
+    return chain;
+  },
+  _deleteChain() {
+    this.currChain.length = 0;
+  },
+  _checkIntegerNumber(num) {
+    return typeof num === 'number' && (num ^ 0) === num;
+  },
+  _getChain() {
+    return this.currChain.join('~~');
   }
 };
 
